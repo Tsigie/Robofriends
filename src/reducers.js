dbfc881d4655fdf58ade7e963913fbec38737590
Tsigie <tktsigie@yahoo.com>
2018-07-18 16:50:22 -0400

    Updated files

--- src/reducers.js
+++ src/reducers.js
@@ -2,13 +2,13 @@ import {
   CHANGE_SEARCH_FIELD,
   REQUEST_ROBOTS_PENDING, REQUEST_ROBOTS_SUCCESS,
   REQUEST_ROBOTS_FAILED
-} from './constants.js'
+} from './constants.js';
 
-const initialState = {
+const initialStateSearch = {
   searchField: ''
 }
 
-export const searchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state=initialStateSearch, action={}) => {
   switch(action.type) {
     case CHANGE_SEARCH_FIELD:
       return Object.assign({}, state, {searchField: action.payload });
@@ -19,19 +19,18 @@ export const searchRobots = (state=initialState, action={}) => {
 }
 
 const initialStateRobots = {
-  isPending: false,
   robots: [],
-  error: ''
+  isPending: true
 }
 
 export const requestRobots = (state=initialStateRobots, action={}) => {
   switch(action.type) {
     case REQUEST_ROBOTS_PENDING:
-      return Object.assign({}, state, { isPending: true})
+      return Object.assign({}, state, { isPending: true })
     case REQUEST_ROBOTS_SUCCESS:
       return Object.assign({}, state, { robots: action.payload, isPending: false })
     case REQUEST_ROBOTS_FAILED:
-      return Object.assign({}, state, { error: action.payload, isPending: false })
+      return Object.assign({}, state, { error: action.payload})
     default:
       return state;
   }
